ef404116be640a27c249b0e918a4c58631dfb9b8
Martina John <martina07j@gmail.com>
2023-07-06 20:16:33 +0530

    details route - trying

--- server/routes/index.js
+++ server/routes/index.js
@@ -10,8 +10,27 @@ const User = require("../models/userModel");
 //     res.send(req.user);
 // });
 
-router.get('/details', ensureAuthenticated, (req, res) => {
-    res.render('details'); // Render the details page if authenticated
-  });
+// router.get('/details', ensureAuthenticated, (req, res) => {
+//     //res.render('details'); // Render the details page if authenticated
+//     res.send("Details page")
+// });
+router.get("/details", (req, res) => {
+  res.send("login page");
+});
 
+router.post("/details", ensureAuthenticated, async(req,res)=>{
+  const user=req.user;
+  const { address, zip } = req.body;
+  const update = await User.findByIdAndUpdate(
+    user._id,
+    {
+      $set: {
+        address: address,
+        zip: zip,
+      },
+    },
+    { new: true }
+  );
+  res.send(update, user);
+})
 module.exports = router
